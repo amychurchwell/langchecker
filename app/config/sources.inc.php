367ca425555703d7fbb15763c7e8489b12ec8017
Amy Churchwell <achurchwell@mozilla.com>
2019-11-14 15:29:22 -0800

    Add firefox/welcome/page4.lang

--- app/config/sources.inc.php
+++ app/config/sources.inc.php
@@ -287,6 +287,13 @@ $mozillaorg_lang = [
             'pt-BR', 'pt-PT', 'ru', 'zh-CN', 'zh-TW',
         ],
     ],
+    'firefox/welcome/page4.lang' => [
+        'priority'          => [
+            1 => ['de', 'fr', 'en-GB', 'en-CA'],
+        ],
+        'deadline'          => '2019-12-11',
+        'supported_locales' => $mozillaorg,
+    ],
     'firefox/whatsnew.lang' => [
         'priority'          => 1,
         'supported_locales' => $firefox_locales,
